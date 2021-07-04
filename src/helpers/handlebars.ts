71e1212bac5dbe81330d2e9713f081fa73286cb1
elhmn <nleme@live.fr>
2021-07-05 00:55:33 +0200

    feat: implement filters

--- src/helpers/handlebars.ts
+++ src/helpers/handlebars.ts
@@ -1,3 +1,7 @@
 export const isLink = (exp: string, got: string): boolean => {
   return exp === got;
 };
+
+export const ifEquals = (a: string, b: string): boolean => {
+  return a === b;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import exphbs from "express-handlebars";
 import * as helpers from "./helpers/handlebars";
-import {getApps} from "./apps";
+import { getApps, Item } from "./apps";
+import Fuse from "fuse.js";
 
 enum E_ENVIRONMENT {
   dev = "dev",
@@ -15,7 +16,7 @@ const app = express();
 const getCurrentYear = () => {
   const d = new Date();
   return d.getFullYear();
-}
+};
 
 //Set view path
 app.set("views", __dirname + "/views");
@@ -31,21 +32,77 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 
+const getItemsMatchingQuery = (
+  query: string,
+  category: string,
+  tag: string,
+  items: any
+): Item[] => {
+  let list = items;
+
+  if (query !== "") {
+    const f: Fuse<Item> = new Fuse(items, {
+      distance: 100,
+      threshold: 0.3,
+      keys: ["description", "title", "tags", "category"],
+    });
+    list = f.search(query)?.map((e: Fuse.FuseResult<Item>): Item => {
+      return e.item;
+    });
+  }
+
+  const filteredByCategory = list.reduce((acc: any, val: any) => {
+    if (
+      category === "any" ||
+      val.category === category
+    ) {
+      return acc.concat(val);
+    }
+    return acc;
+  }, []);
+
+  const filteredByTags = filteredByCategory.reduce((acc: any, val: any) => {
+    if (
+      val.tags.find((t: any) =>  (tag === "any" || t === tag))
+    ) {
+      return acc.concat(val);
+    }
+    return acc;
+  }, []);
+
+  return filteredByTags;
+};
+
 const handleHome = async (req: any, res: any) => {
   const query = req.query.query || "";
+  const selectedCategory = req.query.category || "any";
+  const selectedTag = req.query.tag || "any";
   const apps = getApps()?.items;
+  const filteredApps = getItemsMatchingQuery(
+    query,
+    selectedCategory,
+    selectedTag,
+    apps
+  );
+  const categories = new Set(apps?.map((a) => a.category)).add("any");
+  const tags = new Set(
+    apps?.reduce((acc: any, val: any): any => acc.concat(val.tags), [])
+  ).add("any");
+
   res.render("index", {
     link: "home",
     query,
-    apps,
+    apps: filteredApps,
+    categories: { list: categories, selected: selectedCategory },
+    tags: { list: tags, selected: selectedTag },
     currentYear: getCurrentYear(),
   });
 };
 
 app.get("/", handleHome);
 
-app.use('/views', express.static(__dirname + "/views"))
-app.use('/res', express.static(__dirname + "/res"))
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/res", express.static(__dirname + "/res"));
 app.listen(port, () => {
   console.log(`The application is listening on port ${port}`);
 });
